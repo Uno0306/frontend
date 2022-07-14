08003734c3e288e7ffdd8790744382a4b635e75f
Uno0306 <zion6878@naver.com>
2022-07-14 17:18:04 +0900

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,24 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [msg, setMsg] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/deploy")
+      .then((response) => response.text())
+      .then((promise) => {
+        setMsg(promise);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>프로젝트</h1>
+      <p>{msg}</p>
+    </div>
+  );
 }
 
 export default App;
